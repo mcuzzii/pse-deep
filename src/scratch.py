8868989355b90a74e4baea5a014cabff55963bcf
Marcus Tornea <marcus.tornea@outlook.ph>
2026-04-28 13:35:01 +0800

    fix: deleted unecessary scripts

--- src/scratch.py
+++
@@ -1,8 +0,0 @@
-from processing import DataSource
-from pathlib import Path
-import joblib
-import pandas as pd
-
-df = DataSource()
-df.load_lseg_news(raw_path=Path('../data/raw/news/all_news.xlsx'))
-print(df.df.head(50))
