1b90b4972198ea421cc9edf6f5a93dac1d63abe3
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-01 22:16:21 +0800

    feat: added logs

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ load_dotenv()
 
 def main():
 
+    print("Creating social media data...")
     social_media_data = DataSource()
     social_media_data.create_df(raw_folder_name='social', file_name='social_media', medium='x_posts', text_col='text', date_col='createdAt')
     social_media_data.get_social_sentiment_examples()
 
+    print("Creating news data...")
     lseg_news_data = DataSource()
     lseg_news_data.create_df(raw_folder_name='news', file_name='news', medium='lseg_news')
     lseg_news_data.get_translated_examples()
@@ -27,7 +29,8 @@ def main():
     
     del social_media_data, lseg_news_data
     gc.collect()
-    
+
+    print("Creating stock data...")
     stocks = get_unique_instruments('data/raw/stock')
     for stock in stocks:
         stock_data = DataSource()
@@ -35,6 +38,7 @@ def main():
         del stock_data
         gc.collect()
     
+    print("Creating government bond data...")
     bonds = get_unique_instruments('data/raw/bond')
     for bond in bonds:
         bond_data = DataSource()
@@ -42,34 +46,41 @@ def main():
         del bond_data
         gc.collect()
     
+    print("Creating copper price data...")
     copper = DataSource()
     copper.create_df(raw_folder_name='copper', file_name='copper', medium='copper')
     del copper
     gc.collect()
 
+    print("Creating oil price data...")
     oil = DataSource()
     oil.create_df(raw_folder_name='crude', file_name='lcoc1', medium='oil')
     del oil
     gc.collect()
 
+    print("Creating USD price data...")
     usd = DataSource()
     usd.create_df(raw_folder_name='forex', file_name='usd', medium='fx')
     del usd
     gc.collect()
 
+    print("Creating XAU price data...")
     xau = DataSource()
     xau.create_df(raw_folder_name='xau', file_name='xau', medium='fx')
     del xau
     gc.collect()
 
+    print("Combining government bonds...")
     bond_master = DataSource()
     bond_master.create_df(file_name='bond_master', medium='bonds')
     del bond_master
     gc.collect()
 
+    print("Combining financial instruments...")
     stocks = list(set(stocks) - {'psei', 'psho', 'psse', 'psmo', 'psfi', 'pspr', 'psin'})
     
     for stock in stocks:
+        print(f"Combining instruments for {stock}...")
         stock_data = DataSource()
         stock_data.create_df(file_name=stock, medium='combined', ignore_history=True)
         del stock_data
