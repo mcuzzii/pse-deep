abfdb71f6653975b30201f54f737c8162ce84a7b
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-01 20:39:01 +0800

    fix: configured scratch to save samples

--- scratch.py
+++ scratch.py
@@ -13,15 +13,5 @@ for item in Path('data/processed').glob('*.joblib'):
     print(f'Loading {item.name}...')
     data = joblib.load(item)
     fn = data.file_name
-    columns = data.df.columns
-    targets = [s for s in columns if re.search(r'.+_ad$', s)]
-    if targets:
-        print(f"Found columns {targets} in {fn}.")
-        data.df.drop(columns=targets, inplace=True)
-        columns = data.df.columns
-        if not [s for s in targets if s in columns]:
-            print(f"Successfully removed columns {targets}.")
-            print(f'Saving {fn}...')
-            joblib.dump(data, f'data/processed/{fn}.joblib')
-    else:
-        print(f"Found no '_ad' columns; instead found {data.df.columns.tolist()}")
+    print(f'Saving {fn}.csv...')
+    data.df.head(2000).to_csv(f'data/samples/{fn}.csv')
