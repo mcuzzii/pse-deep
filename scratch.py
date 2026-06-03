e3594c8b4b390461a1351c80b2a62d61dca24617
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-03 19:36:53 +0800

    feat: scratch.py now saves entire final datasets

--- scratch.py
+++ scratch.py
@@ -10,15 +10,7 @@ import gc
 stocks = get_unique_instruments('data/raw/stock')
 stocks = list(set(stocks) - {'psei', 'psho', 'psse', 'psmo', 'psfi', 'pspr', 'psin'})
 
-print("Finalizing datasets...")
 for stock in stocks:
-    stock_data = DataSource()
-    stock_data.create_df(file_name=stock, medium='final', target=30)
-    del stock_data
-    gc.collect()
-
-for stock in stocks:
-    stock_data = DataSource()
-    stock_data.create_df(file_name=stock, medium='final', target=10)
-    del stock_data
-    gc.collect()
+    print(f"Saving {stock}...")
+    data = joblib.load(f'{stock}.joblib')
+    data.df.to_csv(f'data/samples/{stock}.csv')
