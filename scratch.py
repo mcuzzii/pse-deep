0f08d1b7d790766c699cb8a623b0961ad361b131
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-03 16:44:29 +0800

    feat: scratch.py now saves the features_10m dataset

--- scratch.py
+++ scratch.py
@@ -3,12 +3,8 @@ from pathlib import Path
 # Add the 'src' directory to the path
 sys.path.append(str(Path.cwd() / 'src'))
 from processing import DataSource, get_unique_instruments
-import gc
+import joblib
+import pandas as pd
 
-stocks = get_unique_instruments('data/raw/stock')
-stocks = list(set(stocks) - {'psei', 'psho', 'psse', 'psmo', 'psfi', 'pspr', 'psin'})
-
-features_10 = DataSource()
-features_10.create_df(file_name='features_10m', medium='features', target=10, stocks=stocks, ignore_history=True)
-del features_10
-gc.collect()
+data = joblib.load('data/processed/features_10m.joblib')
+pd.concat([data.df.head(1000), data.df.tail(1000)]).to_csv('data/samples/features_10m.csv')
