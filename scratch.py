20be612547b5bab2564f622ea8579f2351658f03
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-03 20:48:15 +0800

    fix: fixed debugging for time indices

--- scratch.py
+++ scratch.py
@@ -17,23 +17,19 @@ common_date_times = None
 for stock in stocks:
     stock_df = joblib.load(f'data/processed/{stock}.joblib')
     dates = stock_df.df.index
+    print(f'Stock: {stock}; length: {len(dates.tolist())}')
     common_date_times = dates if common_date_times is None else common_date_times.intersection(dates)
+    print(f'Common date and time: {len(common_date_times.tolist())}')
 
-print(len(common_date_times.tolist()))
 
 lunch_mask = (
     (common_date_times.time >= pd.Timestamp(f'11:{61 - target}').time()) &
     (common_date_times.time <= pd.Timestamp('12:00').time())
 )
 
-print(lunch_mask[:300])
-
 daily_max = common_date_times.to_series().groupby(common_date_times.date).transform('max')
 last_mask = common_date_times.to_series() > daily_max - pd.Timedelta(minutes=target)
 
-print(daily_max[:300])
-print(last_mask[:300])
-
 filtered_date_times = common_date_times[~lunch_mask & ~last_mask.values]
 
-print(len(filtered_date_time.tolist()))
+print(len(filtered_date_times.tolist()))
