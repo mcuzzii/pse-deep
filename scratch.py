bf6e7496190e838489422df05c927d78a4567f84
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-03 18:36:13 +0800

    feat: added dataset finalization

--- scratch.py
+++ scratch.py
@@ -10,15 +10,15 @@ import gc
 stocks = get_unique_instruments('data/raw/stock')
 stocks = list(set(stocks) - {'psei', 'psho', 'psse', 'psmo', 'psfi', 'pspr', 'psin'})
 
-print("Selecting features...")
-features_30 = DataSource()
-features_30.create_df(file_name='features_30m', medium='features', target=30, stocks=stocks)
-features_30.save_selected_features()
-del features_30
-gc.collect()
+print("Finalizing datasets...")
+for stock in stocks:
+    stock_data = DataSource()
+    stock_data.create_df(file_name=stock, medium='final', target=30)
+    del stock_data
+    gc.collect()
 
-features_10 = DataSource()
-features_10.create_df(file_name='features_10m', medium='features', target=10, stocks=stocks)
-features_10.save_selected_features()
-del features_10
-gc.collect()
+for stock in stocks:
+    stock_data = DataSource()
+    stock_data.create_df(file_name=stock, medium='final', target=10)
+    del stock_data
+    gc.collect()
