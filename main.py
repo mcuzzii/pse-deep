db0f7a19cfedac9712a3002a1da4bc4735dcef42
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-03 16:59:20 +0800

    feat: added save selected features

--- main.py
+++ main.py
@@ -88,12 +88,14 @@ def main():
     
     print("Selecting features...")
     features_30 = DataSource()
-    features_30.create_df(file_name='features_30m', medium='features', target=30, stocks=stocks, ignore_history=True)
+    features_30.create_df(file_name='features_30m', medium='features', target=30, stocks=stocks)
+    features_30.save_selected_features()
     del features_30
     gc.collect()
 
     features_10 = DataSource()
-    features_10.create_df(file_name='features_10m', medium='features', target=10, stocks=stocks, ignore_history=True)
+    features_10.create_df(file_name='features_10m', medium='features', target=10, stocks=stocks)
+    features_10.save_selected_features()
     del features_10
     gc.collect()
 
--- scratch.py
+++ scratch.py
@@ -5,6 +5,17 @@ sys.path.append(str(Path.cwd() / 'src'))
 from processing import DataSource, get_unique_instruments
 import joblib
 import pandas as pd
+import gc
 
-data = joblib.load('data/processed/features_10m.joblib')
-pd.concat([data.df.head(1000), data.df.tail(1000)]).to_csv('data/samples/features_10m.csv')
+print("Selecting features...")
+features_30 = DataSource()
+features_30.create_df(file_name='features_30m', medium='features', target=30, stocks=stocks)
+features_30.save_selected_features()
+del features_30
+gc.collect()
+
+features_10 = DataSource()
+features_10.create_df(file_name='features_10m', medium='features', target=10, stocks=stocks)
+features_10.save_selected_features()
+del features_10
+gc.collect()
