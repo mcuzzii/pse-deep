08b0b1ab44c5c33a6e51142a65111355728dee71
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-09 11:24:35 +0800

    fix: updated data sources to include feature attributes

--- scratch.py
+++ scratch.py
@@ -2,7 +2,7 @@ import sys
 from pathlib import Path
 # Add the 'src' directory to the path
 sys.path.append(str(Path.cwd() / 'src'))
-from processing import DataSource, get_unique_instruments
+from processing import DataSource, get_stocks
 import joblib
 import pandas as pd
 import pandas_ta as ta
@@ -10,12 +10,16 @@ import numpy as np
 import json
 import gc
 
-lseg_news_data = DataSource()
-lseg_news_data.create_df(file_name='news', medium='final_text', ignore_history=True)
-del lseg_news_data
-gc.collect()
+stocks = get_stocks
+print("Finalizing datasets...")
+for stock in stocks:
+    stock_data = DataSource()
+    stock_data.create_df(file_name=stock, medium='final', target=30, ignore_history=True)
+    del stock_data
+    gc.collect()
 
-social_media_data = DataSource()
-social_media_data.create_df(file_name='social_media', medium='final_text', ignore_history=True)
-del social_media_data
-gc.collect()
+for stock in stocks:
+    stock_data = DataSource()
+    stock_data.create_df(file_name=stock, medium='final', target=10, ignore_history=True)
+    del stock_data
+    gc.collect()
