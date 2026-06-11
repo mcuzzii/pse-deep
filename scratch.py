009b03ddd47179bebcd71265b6aa7d7794fa7389
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-11 16:34:33 +0800

    fix: debugging attention

--- scratch.py
+++ scratch.py
@@ -1,18 +1,19 @@
 import torch
+import torch.nn as nn
+import sys
+from pathlib import Path
+sys.path.append(str(Path.cwd() / 'src'))
+from models import AttentionBlock
 
-all_masked_y = torch.tensor([0, 1, 0, 0, 1, 0, 1, 1], dtype=bool)             
-mask_y = torch.tensor([
-    [1, 0, 0],
-    [0, 1, 0],
-    [1, 1, 0],
-    [0, 1, 1],
-    [1, 0, 1],
-    [1, 1, 1],
-    [0, 0, 0],
-    [0, 0, 1]
-])         # (b * n,)
-safe_mask_y = mask_y.clone()                                           # (b * n, y_seq)
-safe_mask_y[all_masked_y, 0] = False
+model = AttentionBlock(
+    embedding_dim=512,
+    num_heads=8,
+    is_causal=True
+)
 
-print(safe_mask_y)
-print(mask_y)
+x = torch.randn(3, 10, 512)
+x_mask = torch.rand(3, 10) >= 0.5
+
+out = model(x, x, mask, mask)
+
+print(out)
