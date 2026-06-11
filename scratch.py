ffbb26d4e78269b6511be19401f2ea8852653237
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-11 19:42:09 +0800

    feat: debugging attention

--- scratch.py
+++ scratch.py
@@ -5,17 +5,24 @@ from pathlib import Path
 sys.path.append(str(Path.cwd() / 'src'))
 from models import AttentionBlock
 
-model = AttentionBlock(
+model = nn.MultiheadAttention(
     embedding_dim=512,
     num_heads=8,
-    is_causal=True
+    batch_first=True
 )
 
-x = torch.randn(3, 2, 4, 512)
-y = torch.randn(3, 2, 5, 512)
-y_mask = (torch.rand(3, 2, 5) >= 0.5)
+x = torch.randn(3, 2, 4, 512).flatten(0, 1)
+y = torch.randn(3, 2, 5, 512).flatten(0, 1)
+y_mask = (torch.rand(3, 2, 5) >= 0.5).flatten(0, 1)
 
-out = model(x, y, mask_y=y_mask)[1].detach().numpy()
+x_sz = x.size(1)
+y_sz = y.size(1)
+attn_mask = torch.triu(
+    torch.ones(x_sz, y_sz, dtype=torch.bool, device=x.device),
+    diagonal=1
+)
+
+out = model(x, y, y, key_padding_mask=y_mask, attn_mask=attn_mask, need_weights=True, average_attn_weights=False)[1].detach().numpy()
 
 for i in range(3):
     for j in range(2):
