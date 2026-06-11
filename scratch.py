d7de7a232f7479776542084b582d730b7eeafd9c
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-12 03:50:20 +0800

    fix: fixed attention bugs

--- scratch.py
+++ scratch.py
@@ -10,13 +10,14 @@ model = AttentionBlock(
     num_heads=8
 )
 
-x = torch.randn(3, 2, 4, 512)
+x = torch.randn(3, 2, 6, 512)
 y = torch.randn(3, 2, 5, 512)
 
-tx = torch.arange(0, 4).unsqueeze(0).unsqueeze(0).expand(3, 2, 4)
+tx = torch.arange(0, 4).unsqueeze(0).unsqueeze(0).expand(3, 2, 6)
 ty = torch.arange(0, 5).unsqueeze(0).unsqueeze(0).expand(3, 2, 5)
 
-y_mask = (torch.rand(3, 2, 5) >= 0.5)
+x_mask = (torch.rand(3, 2, 6) >= 0.75)
+y_mask = (torch.rand(3, 2, 5) >= 0.75)
 
 out = model(tx, ty, x, y, mask_y=y_mask)
 
