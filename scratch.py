c62c7866fd15b0103a2199d653afdf5ffe4d5c46
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-11 17:51:44 +0800

    fix: debugging attention

--- scratch.py
+++ scratch.py
@@ -11,8 +11,8 @@ model = nn.MultiheadAttention(
     batch_first=True
 )
 
-x = torch.randn(3, 2, 4, 512)
-x_mask = torch.rand(3, 2, 4) >= 0.5
+x = torch.randn(3, 2, 4, 512).flatten(0, 1)
+x_mask = (torch.rand(3, 2, 4) >= 0.5).flatten(0, 1)
 
 out = model(x, x, x, key_padding_mask=x_mask, need_weights=True, average_attn_weights=False)[1]
 
