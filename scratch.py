90c244a5d3e189adfab9469c7e7a3b80a590129c
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-12 05:48:49 +0800

    feat: scratch.py to evaluate loss

--- scratch.py
+++ scratch.py
@@ -1,28 +1,6 @@
 import torch
-import torch.nn as nn
-import sys
-from pathlib import Path
-sys.path.append(str(Path.cwd() / 'src'))
-from models import AttentionBlock
 
-model = AttentionBlock(
-    embedding_dim=512,
-    num_heads=8
-)
-
-x = torch.randn(3, 2, 6, 512)
-y = torch.randn(3, 2, 5, 512)
-
-tx = torch.arange(0, 6).unsqueeze(0).unsqueeze(0).expand(3, 2, 6)
-ty = torch.arange(0, 5).unsqueeze(0).unsqueeze(0).expand(3, 2, 5)
-
-x_mask = (torch.rand(3, 2, 6) >= 0.5)
-y_mask = (torch.rand(3, 2, 5) >= 0.55)
-
-out = model(tx, ty, x, y, mask_x=x_mask, mask_y=y_mask)
-
-for i in range(3):
-    for j in range(2):
-        print(out[i, j, 0])
-        print(f'{y_mask[i, j]}\n')
-        print(out.shape)
+d = torch.load('experiments/stock_transformer_30/stock_transformer_30.pt')
+print(d['train_losses'])
+print(d['val_losses'])
+print(d['total_loss'])
