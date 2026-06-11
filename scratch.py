f291bf1527d0c47f672220e0a946613946cf0532
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-11 17:51:03 +0800

    fix: debugging attention

--- scratch.py
+++ scratch.py
@@ -5,17 +5,16 @@ from pathlib import Path
 sys.path.append(str(Path.cwd() / 'src'))
 from models import AttentionBlock
 
-model = AttentionBlock(
-    embedding_dim=512,
+model = nn.MultiheadAttention(
+    embed_dim=512,
     num_heads=8,
-    is_causal=True
+    batch_first=True
 )
 
 x = torch.randn(3, 2, 4, 512)
 x_mask = torch.rand(3, 2, 4) >= 0.5
 
-
-out = model(x, x)[1]
+out = model(x, x, x, key_padding_mask=x_mask, need_weights=True, average_attn_weights=False)[1]
 
 print(out.detach().numpy())
 print(out.detach().numpy().shape)
