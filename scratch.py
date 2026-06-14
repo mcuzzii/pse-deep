eb3ce02cd104ca7d61426f8264d045251ee8e112
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-14 17:57:56 +0800

    testing early stopping checkpointing

--- scratch.py
+++ scratch.py
@@ -1,6 +1,6 @@
 import torch
 
-d = torch.load('experiments/stock_transformer_30/stock_transformer_30.pt', map_location=torch.device('cpu'))
+d = torch.load('experiments/stock_transformer_30/stock_transformer_30.pt', map_location=torch.device('cpu'), weights_only=False)
 print(d['train_losses'])
 print(d['val_losses'])
 print(d['total_loss'])
