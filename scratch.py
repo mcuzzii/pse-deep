a8f669e8b8ff29ea11ec03b757ba26cb0bacc9eb
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-14 18:00:58 +0800

    testing early stopping checkpointing

--- scratch.py
+++ scratch.py
@@ -1,4 +1,10 @@
 import torch
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path.cwd() / 'src'))
+
+from experiments import EarlyStopping, SigmaAnnealer
 
 d = torch.load('experiments/stock_transformer_30/stock_transformer_30.pt', map_location=torch.device('cpu'), weights_only=False)
 print(d['train_losses'])
