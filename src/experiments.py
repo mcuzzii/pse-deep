b4b7d9139c29df4246a6768f2211d1ef72f1aef4
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-15 00:37:43 +0800

    fix: debugging batch_size

--- src/experiments.py
+++ src/experiments.py
@@ -18,6 +18,7 @@ from processing import DataSource, get_stocks, get_text_window, get_elapsed_time
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def collate_fn(batch):
+    print(len(batch))
     args = list(zip(*batch))
     n = len(args)
 
@@ -93,6 +94,7 @@ class StockNewsTransformerDataset(StockTransformerDataset):
         self.time_vec_input = time_vec_input
     
     def __len__(self):
+        print(super().__len__())
         return super().__len__()
     
     def __getitem__(self, idx):
