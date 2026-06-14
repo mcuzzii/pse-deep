ec486c9e5544022a43912e5c95d08c3fc6dbd428
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-15 00:35:12 +0800

    fix: debugged batch_size

--- src/experiments.py
+++ src/experiments.py
@@ -93,7 +93,7 @@ class StockNewsTransformerDataset(StockTransformerDataset):
         self.time_vec_input = time_vec_input
     
     def __len__(self):
-        return super().__init__()
+        return super().__len__()
     
     def __getitem__(self, idx):
         t, x, y = super().__getitem__(idx)
