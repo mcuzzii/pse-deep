180d5f673e30a8ce2453ecc9d5095dd9688581a8
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-15 18:18:55 +0800

    feat: for news transformer: k = 5 and num_samples = 150

--- main.py
+++ main.py
@@ -171,7 +171,7 @@ def main():
         embedding_dim=128,
         temporal_embedding_dim=16,
         K=5,
-        num_samples=200,
+        num_samples=150,
         sigma=5e-2,
         dropout=0.1
     )
