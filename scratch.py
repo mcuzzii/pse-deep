4b1609199a8bbae046f73f49b9f963cf9e2f9f6c
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-16 06:38:59 +0800

    feat: plot losses

--- scratch.py
+++ scratch.py
@@ -9,6 +9,13 @@ from experiments import Experiment, collate_fn
 
 d = torch.load('experiments/stock_news_transformer_30/checkpoints/stock_news_transformer_30.pt', map_location='cpu', weights_only=False)
 
-print(d['train_losses'])
-print(d['val_losses'])
-print(d['total_loss'])
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(8, 5))
+plt.plot(d['train_losses'], label='Train loss')
+plt.plot(d['val_losses'], label='Val loss', linestyle='--')
+plt.xlabel('Validation checkpoint')
+plt.ylabel('Loss')
+plt.legend()
+plt.tight_layout()
+plt.show()
