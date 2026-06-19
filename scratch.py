a6dad6268d8315d2bef689641159e8c27aa546f1
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-19 19:06:25 +0800

    feat: testing social media

--- scratch.py
+++ scratch.py
@@ -9,6 +9,7 @@ from processing import DataSource
 social_final = DataSource()
 social_final.create_df('social_media_copy')
 social_final.df = social_final.df.set_index('created_at')
-social_final._finalized_text(ignore_history=False)
+social_final._finalized_text(ignore_history=True)
 print(social_final.df.index)
 print(social_final.df.columns)
+joblib.dump(social_final, 'data/processed/social_media.joblib')
