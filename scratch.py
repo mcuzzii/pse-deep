b0508189f91a300a0361807afe99bcf174f3c52c
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-19 18:58:00 +0800

    feat: testing social media

--- scratch.py
+++ scratch.py
@@ -4,10 +4,5 @@ import joblib
 
 sys.path.append(str(Path.cwd() / 'src'))
 
-from processing import DataSource
-
-social_final = DataSource()
-social_final.create_df('social_media')
-social_final.df = social_final.df.set_index('created_at')
-social_final._finalized_text()
-joblib.dump(social_final, 'social_media.joblib')
+social_final = joblib.load('data/processed/social_media.joblib')
+print(social_final.df.index)
