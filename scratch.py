72e6a10e3b9303b6c8664863bd515869b78cc9b5
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-19 19:13:04 +0800

    feat: testing text indicators

--- scratch.py
+++ scratch.py
@@ -7,9 +7,5 @@ sys.path.append(str(Path.cwd() / 'src'))
 from processing import DataSource
 
 social_final = DataSource()
-social_final.create_df('social_media_copy')
-social_final.df = social_final.df.set_index('created_at')
-social_final._finalized_text(ignore_history=True)
-print(social_final.df.index)
-print(social_final.df.columns)
-joblib.dump(social_final, 'data/processed/social_media.joblib')
+social_final.create_df('social_media')
+social_final._text_indicators()
