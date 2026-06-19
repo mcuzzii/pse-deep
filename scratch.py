e338bf95a2c04cf496ca3b7be1ecb86c13458337
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-19 23:00:35 +0800

    inspecting news

--- scratch.py
+++ scratch.py
@@ -8,4 +8,12 @@ from processing import DataSource
 
 news = DataSource()
 news.create_df('news')
-print(news.df.index.date)
+
+import numpy as np
+
+values, counts = np.unique(news.df.index.date, return_counts=True)
+d = dict(zip(values, counts))
+
+from collections import Counter
+
+print(Counter(d.values()))
