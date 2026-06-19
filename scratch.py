4b1711ac3766e0b9166555f5621094ac87b96b46
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-20 07:34:24 +0800

    feat: testing Experiment

--- scratch.py
+++ scratch.py
@@ -6,18 +6,7 @@ import gc
 sys.path.append(str(Path.cwd() / 'src'))
 
 from processing import DataSource, get_stocks
+from experiments import Experiment
 
-stocks = get_stocks()
-
-print("Finalizing datasets...")
-for stock in stocks:
-    stock_data = DataSource()
-    stock_data.create_df(file_name=stock, medium='final', target=30, ignore_history=True)
-    del stock_data
-    gc.collect()
-
-for stock in stocks:
-    stock_data = DataSource()
-    stock_data.create_df(file_name=stock, medium='final', target=10, ignore_history=True)
-    del stock_data
-    gc.collect()
+test_experiment = Experiment('text_experiment')
+test_experiment.test()
