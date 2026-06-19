5e042bf0a7d1d01c68aaabbbbb8484addb1caafd
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-20 01:22:35 +0800

    reprocessing final datasets

--- scratch.py
+++ scratch.py
@@ -5,24 +5,32 @@ import gc
 
 sys.path.append(str(Path.cwd() / 'src'))
 
-from processing import DataSource
+from processing import DataSource, get_unique_instruments
 
-social_indicators_30 = DataSource()
-social_indicators_30.create_df('social_media', medium='social_indicators', target=30, ignore_history=True)
-del social_indicators_30
+stocks = get_unique_instruments('data/raw/stock')
+
+print("Selecting features...")
+features_30 = DataSource()
+features_30.create_df(file_name='features_30m', medium='features', target=30, stocks=stocks, ignore_history=True)
+features_30.save_selected_features()
+del features_30
 gc.collect()
 
-social_indicators_10 = DataSource()
-social_indicators_10.create_df('social_media', medium='social_indicators', target=10, ignore_history=True)
-del social_indicators_10
+features_10 = DataSource()
+features_10.create_df(file_name='features_10m', medium='features', target=10, stocks=stocks, ignore_history=True)
+features_10.save_selected_features()
+del features_10
 gc.collect()
 
-news_indicators_30 = DataSource()
-news_indicators_30.create_df('news', medium='news_sentiment', target=30)
-del news_indicators_30
-gc.collect()
+print("Finalizing datasets...")
+for stock in stocks:
+    stock_data = DataSource()
+    stock_data.create_df(file_name=stock, medium='final', target=30)
+    del stock_data
+    gc.collect()
 
-news_indicators_10 = DataSource()
-news_indicators_10.create_df('news', medium='news_sentiment', target=10, ignore_history=True)
-del news_indicators_10
-gc.collect()
+for stock in stocks:
+    stock_data = DataSource()
+    stock_data.create_df(file_name=stock, medium='final', target=10)
+    del stock_data
+    gc.collect()
