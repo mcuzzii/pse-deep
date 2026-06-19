b33b49ce969586d7b3113da04284809458cc51d3
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-20 01:54:15 +0800

    feat: reprocessing final datasets

--- scratch.py
+++ scratch.py
@@ -9,19 +9,6 @@ from processing import DataSource, get_stocks
 
 stocks = get_stocks()
 
-print("Selecting features...")
-features_30 = DataSource()
-features_30.create_df(file_name='features_30m', medium='features', target=30, stocks=stocks, ignore_history=True)
-features_30.save_selected_features()
-del features_30
-gc.collect()
-
-features_10 = DataSource()
-features_10.create_df(file_name='features_10m', medium='features', target=10, stocks=stocks, ignore_history=True)
-features_10.save_selected_features()
-del features_10
-gc.collect()
-
 print("Finalizing datasets...")
 for stock in stocks:
     stock_data = DataSource()
