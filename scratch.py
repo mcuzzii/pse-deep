528f5593f529dfd30456f1586e60e3e342685283
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-19 19:04:50 +0800

    feat: testing social media

--- scratch.py
+++ scratch.py
@@ -9,6 +9,6 @@ from processing import DataSource
 social_final = DataSource()
 social_final.create_df('social_media_copy')
 social_final.df = social_final.df.set_index('created_at')
-social_final._finalized_text()
+social_final._finalized_text(ignore_history=False)
 print(social_final.df.index)
 print(social_final.df.columns)
