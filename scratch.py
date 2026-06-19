bd777d24c448fc39e6d439e5d8b02bba513eaf58
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-19 22:26:31 +0800

    inspecting social_media_30m

--- scratch.py
+++ scratch.py
@@ -7,4 +7,5 @@ sys.path.append(str(Path.cwd() / 'src'))
 from processing import DataSource
 
 social_final = DataSource()
-social_final.create_df('social_media', medium='social_indicators', target=30)
+social_final.create_df('social_media_30m')
+social_final.df.to_csv('data/samples/social_media_30m_sample.csv')
