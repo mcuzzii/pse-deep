d3bb380ba723dd7eba48b17550e2f4d3595f0f24
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-20 00:03:47 +0800

    inspecting news

--- scratch.py
+++ scratch.py
@@ -7,6 +7,5 @@ sys.path.append(str(Path.cwd() / 'src'))
 from processing import DataSource
 
 news_indicators_30 = DataSource()
-news_indicators_30.create_df('news', medium='news_sentiment', target=30)
-del news_indicators_30
-gc.collect()
+news_indicators_30.create_df('news_30m')
+news_indicators_30.df.to_csv('data/samples/news_30m.csv')
