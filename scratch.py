369b643413b4d8e34cb0124d199e4d11b18adc8b
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-19 22:52:20 +0800

    inspecting news

--- scratch.py
+++ scratch.py
@@ -8,4 +8,4 @@ from processing import DataSource
 
 news = DataSource()
 news.create_df('news')
-print(news.index.days.value_counts().value_counts())
+print(news.df.index.days.value_counts().value_counts())
