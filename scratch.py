b096746aa7e057e1ffacf895baf2b706a769fac8
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-20 00:09:31 +0800

    generating all text features

--- scratch.py
+++ scratch.py
@@ -6,6 +6,22 @@ sys.path.append(str(Path.cwd() / 'src'))
 
 from processing import DataSource
 
+social_indicators_30 = DataSource()
+social_indicators_30.create_df('social_media', medium='social_indicators', target=30, ignore_history=True)
+del social_indicators_30
+gc.collect()
+
+social_indicators_10 = DataSource()
+social_indicators_10.create_df('social_media', medium='social_indicators', target=10, ignore_history=True)
+del social_indicators_10
+gc.collect()
+
 news_indicators_30 = DataSource()
-news_indicators_30.create_df('news_30m')
-news_indicators_30.df.to_csv('data/samples/news_30m.csv')
+news_indicators_30.create_df('news', medium='news_sentiment', target=30)
+del news_indicators_30
+gc.collect()
+
+news_indicators_10 = DataSource()
+news_indicators_10.create_df('news', medium='news_sentiment', target=10, ignore_history=True)
+del news_indicators_10
+gc.collect()
