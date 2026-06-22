b7b1bd8d4be6ec515ddb2858d6df2a8cd0bf0d56
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-23 04:36:52 +0800

    feat: testing data alignment

--- scratch.py
+++ scratch.py
@@ -4,4 +4,4 @@ import matplotlib.pyplot as plt
 
 test_tensor = torch.load('experiments/stock_mlp_10/test_outputs.pt', map_location='cpu', weights_only=False)
 
-print(test_tensor.shape)
+print(test_tensor['test_logit_scores'].shape)
