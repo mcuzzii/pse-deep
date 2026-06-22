e8f370a49fcadc27dbd91deec8b13ebca02424b3
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-22 14:39:33 +0800

    feat: testing class weights

--- scratch.py
+++ scratch.py
@@ -1,4 +1,10 @@
 import torch
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path.cwd() / 'src'))
+
+from experiments import EarlyStopping
 
 model = torch.load('experiments/stock_transformer_10/stock_transformer_10.pt', map_location='cpu', weights_only=False)
 
