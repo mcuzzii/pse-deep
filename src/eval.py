7a6a8d68d17ed403c1abef4d168f20f13ea8a159
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-23 13:44:15 +0800

    feat: testing data alignment

--- src/eval.py
+++ src/eval.py
@@ -1,6 +1,8 @@
 import torch
+import torch.nn as nn
 from pathlib import Path
 import pandas as pd
+from river.drift import ADWIN
 
 class Eval:
     def __init__(self):
@@ -31,3 +33,24 @@ class Eval:
         df.to_csv(self.results_path / 'model_scores.csv')
 
         return df
+    
+    def compute_model_drift(self):
+        for dir in self.experiments_path.iterdir():
+
+            if dir.name in ('data', 'experiments', 'results'):
+                continue
+
+            test_outputs = dir / 'test_outputs.pt'
+            out = torch.load(test_outputs, map_location=torch.device('cpu'), weights_only=False)
+
+            logit_scores = out['test_logit_scores']            # N, S, 2 for transformers, N, 1, 2 for mlp
+            if 'mlp' in dir.name:
+                logit_scores = logit_scores.squeeze(1).reshape(30, logit_scores.shape[0] // 30, -1).transpose(0, 1)
+            
+            targets = out['test_all_targets']                  # N, S
+
+            criterion = nn.CrossEntropyLoss(reduction='none')
+
+            loss = criterion(logit_scores.permute(0, 2, 1), targets) # N, S
+
+            for s in loss.shape[1]:
