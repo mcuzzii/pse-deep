5a03f6c6b40a32d7ecb78ad188200cd2848c44a5
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-23 17:23:35 +0800

    feat: testing adwin

--- scratch.py
+++ scratch.py
@@ -1,8 +1,12 @@
 import torch
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
+from pathlib import Path
 
-test_tensor = torch.load('experiments/stock_mlp_10/test_outputs.pt', map_location='cpu', weights_only=False)
-test_tensor['test_all_targets'] = test_tensor['test_all_targets'].squeeze(1).reshape(30, test_tensor['test_all_targets'].shape[0] // 30).transpose(0, 1)
+sys.path.append(str(Path.cwd() / 'src'))
 
-print(test_tensor['test_all_targets'].shape)
+from eval import Eval
+
+eval = Eval()
+eval.compute_model_drift()
--- src/eval.py
+++ src/eval.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 from pathlib import Path
 import pandas as pd
 from river.drift import ADWIN
+import numpy as np
+from tqdm import tqdm
 
 class Eval:
     def __init__(self):
@@ -43,14 +45,37 @@ class Eval:
             test_outputs = dir / 'test_outputs.pt'
             out = torch.load(test_outputs, map_location=torch.device('cpu'), weights_only=False)
 
-            logit_scores = out['test_logit_scores']            # N, S, 2 for transformers, N, 1, 2 for mlp
+            logit_scores = out['test_logit_scores']                         # N, S, 2 for transformers, N*S, 1, 2 for mlp
+            targets = out['test_all_targets']                               # N, S for transformers, N*S, 1 for mlp
             if 'mlp' in dir.name:
                 logit_scores = logit_scores.squeeze(1).reshape(30, logit_scores.shape[0] // 30, -1).transpose(0, 1)
-            
-            targets = out['test_all_targets']                  # N, S
+                targets = targets.squeeze(1).reshape(30, targets.shape[0] // 30).transpose(0, 1)
 
             criterion = nn.CrossEntropyLoss(reduction='none')
+            loss = criterion(logit_scores.permute(0, 2, 1), targets)        # N, S
 
-            loss = criterion(logit_scores.permute(0, 2, 1), targets) # N, S
+            detectors = [ADWIN() for _ in range(loss.shape[1])]
+            windows = [[] for _ in range(loss.shape[1])]
+            means = torch.zeros_like(loss)
+            width_histories = [[] for _ in range(loss.shape[1])]
 
-            for s in loss.shape[1]:
+            for s in range(loss.shape[1]):
+                for n in tqdm(range(loss.shape[0])):
+                    val = loss[n, s].item()
+                    detectors[s].update(val)
+
+                    windows[s].append(val)
+                    windows[s] = windows[s][-detectors[s].width:]
+                    means[n, s] = sum(windows[s]) / len(windows[s])
+                    width_histories[s].append(detectors[s].width)
+
+            drift_scores = ((loss - means) ** 2).mean(dim=0)
+            drift_scores_norm = (drift_scores - drift_scores.min()) / (drift_scores.max() - drift_scores.min())
+            avg_widths = torch.tensor([sum(w) / len(w) for w in width_histories])
+            avg_widths_norm = (avg_widths - avg_widths.min()) / (avg_widths.max() - avg_widths.min())
+            drift_from_width = 1 - avg_widths_norm
+
+            print(f'Drift scores: {drift_scores_norm}')
+            print(f'Drift from width: {drift_from_width}')
+
+            print(f'Corr: {np.corrcoef(drift_scores_norm.numpy(), drift_from_width.numpy())}')
