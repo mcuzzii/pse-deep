f29015f49712a9e070020455bbfe5acad9f74c3b
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-23 14:52:20 +0800

    feat: testing data alignment

--- scratch.py
+++ scratch.py
@@ -3,6 +3,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 test_tensor = torch.load('experiments/stock_mlp_10/test_outputs.pt', map_location='cpu', weights_only=False)
-test_tensor['test_logit_scores'] = test_tensor['test_logit_scores'].squeeze(1).reshape(30, test_tensor['test_logit_scores'].shape[0] // 30, -1).transpose(0, 1)
+test_tensor['test_all_targets'] = test_tensor['test_all_targets'].squeeze(1).reshape(30, test_tensor['test_all_targets'].shape[0] // 30).transpose(0, 1)
 
-print(test_tensor['test_logit_scores'].shape)
+print(test_tensor['test_all_targets'].shape)
