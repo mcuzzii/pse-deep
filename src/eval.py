988e487ef0fb1df566df9a86620fc0aa8850833b
Marcus Tornea <marcus.tornea@outlook.ph>
2026-06-30 07:57:25 +0800

    fix: debugging interpret_trading_sim

--- src/eval.py
+++ src/eval.py
@@ -1063,6 +1063,7 @@ class Eval:
 
             final_returns_per_offset = []
             num_offset = len(value.items())
+            print(key)
             for i, (offset_key, offset) in enumerate(value.items()):
                 reference = pd.read_csv(
                     f'experiments/results/trading_sim/close_prices/{pred_horizon}_{offset_key}.csv',
@@ -1072,6 +1073,8 @@ class Eval:
                 reference = reference.loc[reference.index.get_level_values(0).isin(ts)]
 
                 offset_tensor = torch.stack(list(offset.values()), dim=0)               # k (N,) -> (k, N)
+                print(offset_tensor)
+                print(offset_tensor.shape)
                 if i % (num_offset // 10) == 0:
                     final_returns_per_offset.append(offset_tensor[:, -1])               # k
 
@@ -1227,6 +1230,8 @@ class Eval:
 
             fig.savefig(self.results_path / 'trading_sim' / 'baseline.png', dpi=300, bbox_inches='tight')
             plt.close(fig)
+
+            final_returns_per_model['deep_learning'] = final_returns_per_model.pop(score)
 
             run_wilcoxon_table(final_returns_per_model, 'cum_profit', self.results_path / 'baseline_comparison')
     
