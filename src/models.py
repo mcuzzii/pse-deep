406e62663f635383813d8e6bd2888628cc9fe720
mcuzzii <marcus.tornea@outlook.ph>
2026-07-07 19:37:09 +0000

    feat: removed SHAP aggregation

--- src/models.py
+++ src/models.py
@@ -761,7 +761,6 @@ class GroupSHAPWrapper(nn.Module):
         down_idx = 1 - up_idx
 
         score = logits[..., up_idx] - logits[..., down_idx]
-        score = score.mean(dim=1)
 
         return score
 
