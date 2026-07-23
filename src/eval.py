5d017d0d945cfa8a3bbef65393a5c06c0dff6649
Marcus Tornea <marcus.tornea@outlook.ph>
2026-07-23 17:12:55 +0800

    feat: reworked SHAP plots

--- src/eval.py
+++ src/eval.py
@@ -2955,7 +2955,7 @@ class Eval:
 
             generate_shap_beeswarm_plots(
                 df,
-                output_dir=f"experiments/results/shap_analysis/{name}_shap_beeswarms",
+                output_dir=f"experiments/results/shap_analysis/{name}_shap_beeswarms_all_time",
                 time_unit="all_time",       # "all_time", "week", or "day"
                 trim_quantile=0.995,        # removes largest 0.5% |SHAP| for display
                 max_points_overview=1_500,
@@ -2964,16 +2964,16 @@ class Eval:
 
             generate_shap_beeswarm_plots(
                 df,
-                output_dir=f"experiments/results/shap_analysis/{name}_shap_beeswarms",
+                output_dir=f"experiments/results/shap_analysis/{name}_shap_beeswarms_week",
                 time_unit="week",       # "all_time", "week", or "day"
-                trim_quantile=0.995,        # removes largest 0.5% |SHAP| for display
+                trim_quantile=0.95,        # removes largest 0.5% |SHAP| for display
                 max_points_overview=1_500,
                 max_points_per_setting=750,
             )
 
             generate_shap_beeswarm_plots(
                 df,
-                output_dir=f"experiments/results/shap_analysis/{name}_shap_beeswarms",
+                output_dir=f"experiments/results/shap_analysis/{name}_shap_beeswarms_day",
                 time_unit="day",       # "all_time", "week", or "day"
                 trim_quantile=0.995,        # removes largest 0.5% |SHAP| for display
                 max_points_overview=1_500,
