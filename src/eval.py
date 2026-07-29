a8cb17f4b5b724e62d897dfb6956bfe7e0e57b7b
Marcus Tornea <marcus.tornea@outlook.ph>
2026-07-29 11:36:00 +0800

    feat: testing attention plots

--- src/eval.py
+++ src/eval.py
@@ -2876,6 +2876,15 @@ class Eval:
                 num_samples=500,
                 sigma=5e-2,
             )
+            experiment.train(
+                num_epochs=50,
+                batch_size=2 if transformer else 32,
+                accumulation_steps=16 if transformer else 1,
+                lr=1e-4,
+                val_every=lambda x: (8 * x) ** 2,
+                patience=20,
+                sigma_end=1e-5
+            )
             experiment.run_testing(force=True, evaluator=self)
 
         for dir in self.experiments_path.iterdir():
