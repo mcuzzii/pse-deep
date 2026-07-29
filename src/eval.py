24ceebc8a9f6a0704b22e8012af9617bc9524526
Marcus Tornea <marcus.tornea@outlook.ph>
2026-07-29 12:24:57 +0800

    feat: testing attention plots

--- src/eval.py
+++ src/eval.py
@@ -2987,8 +2987,7 @@ class Eval:
                 txt_attn = snapshot[attn][:, -1, :].unsqueeze(-1)                       # S, K, 1
 
                 snapshot[ind] = torch.cat([txt_attn, text[closest_idx, :-1]], dim=-1)                           # S, K, 1+1+E
-                snapshot[f"{ind}tr"] = torch.cat([txt_attn, text[closest_idx, -1].unsqueeze(-1)])               # S, K, 2
-                print(snapshot[f'{ind}tr'].shape)
+                snapshot[f"{ind}tr"] = torch.cat([txt_attn, text[closest_idx, -1].unsqueeze(-1)], dim=-1)       # S, K, 2
 
                 reorder = torch.argmax(self.stock_map[self.dir_name]['stock_map'], dim=-1).cpu()
                 snapshot[ind] = snapshot[ind][reorder]
