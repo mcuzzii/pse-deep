6406cf2d06c06184d09f3b0cd5f89a21005e98c5
Marcus Tornea <marcus.tornea@outlook.ph>
2026-07-31 05:20:47 +0800

    feat: retraining models

--- main.py
+++ main.py
@@ -171,7 +171,7 @@ def run_experiments():
     for transformer in (True, False):
         for social in (True, False):
             for news in (True, False):
-                for pred_30 in (False, True):
+                for pred_30 in (True, False):
 
                     news_prefix = 'news_' if news else ''
                     social_prefix = 'social_' if social else ''
